6303b8bdc3d868d6d96d4e9973804debd7501484
liobouchan <leo.bouchan@gmail.com>
2016-01-13 12:24:16 -0600

    Ya funciona el PIN de recogida

--- TES/www/js/indexAppGoogleMap.js
+++ TES/www/js/indexAppGoogleMap.js
@@ -6,18 +6,30 @@ function initialize(){
     zoom:13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
+  var image = 'icons/icono.png';
+
   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
   var marker = new google.maps.Marker({
+    draggable: true,
+    animation: google.maps.Animation.DROP,
     position:myCenter,
   });
 
   marker.setMap(map);
 
   var infowindow = new google.maps.InfoWindow({
-    content:"¿A donde te llevamos? , estás aquí."
+    content:"¿Donde te recogemos? , estás aquí."
   });
 
   infowindow.open(map,marker);
+  // marker.addListener('click', toggleBounce);
+  // function toggleBounce() {
+  // if (marker.getAnimation() !== null) {
+  //   marker.setAnimation(null);
+  // } else {
+  //   marker.setAnimation(google.maps.Animation.BOUNCE);
+  // }
+  // }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
